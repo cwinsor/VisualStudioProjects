4cf52aadca07e3fc36d22400b49c52ace60e7a8f
Chris Winsor <CWinsor@rapidmicro.com>
2018-01-07 16:56:40 -0500

    new

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SpeechSynthesizer ss = new SpeechSynthesizer();
+            ss.Speak("hello!");
+
+
+
+
+
+
+        }
+    }
+}
