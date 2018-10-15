2728b1f7e9d3449cc0d4465925aaadbd3f8fe7f3
Chris Winsor <CWinsor@rapidmicro.com>
2018-10-14 21:57:09 -0400

    minor change to ConsoleApp1 - test writeline

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 
+            System.Console.WriteLine("hello!!!");
        //    Crud crud = new Crud();
        //     crud.PopUpNow();
        //     String s = crud.GetInput();
